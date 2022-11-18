5fef52a6e7c1b66fd17eb53173d5e01a3ddc9c81
Davi Oliveira <davi.comp.oliveira@gmail.com>
2022-11-18 17:39:51 -0300

    feat(product-update): added form already filled on component mount event

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
   styleUrls: ['./product-update.component.scss']
 })
-export class ProductUpdateComponent {
+export class ProductUpdateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null as any
+  }
+  
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+
+    this.productService.readById(id || '').subscribe((product) => {
+      this.product = product
+    })
+  }
+
+  onUpdate(): void {
+
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/products'])
+  }
 
 }
